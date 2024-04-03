a87114b6850b6202eebc7db22e8d6d45a461fcb9
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-04-03 17:58:25 +0300

    Create files structure

---
+++ src/app/pages/home/components/main-nav/components/filter-list/filter-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-list',
+  templateUrl: './filter-list.component.html',
+  styleUrl: './filter-list.component.scss'
+})
+export class FilterListComponent {
+
+}
